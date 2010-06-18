978330735d939e8418c23edb2303e80860b18f61
Diogok <manifesto@manifesto.blog.br>
2010-06-18 14:44:17 -0300

    update query model

--- DigirQuery.php
+++ DigirQuery.php
@@ -2,11 +2,133 @@
 include "SimpleDigir.php" ;
 
 class DigirQuery {
+
+    public $sql = "";
+    public $fields = array();
+    public $from = array();
+    public $where = array();
     
-    private function __construct() {
+    private function __construct($sql){
+        $sql = str_replace('\"',"'",$sql);
+        $sql = str_replace('"',"'",$sql);
+        $sql = str_replace('`',"'",$sql);
+        $sql = str_replace('´',"'",$sql);
+        $this->sql = $sql;
+        $this->fields = $this->parseFields($sql);
+        $this->from = $this->parseFrom($sql);
+        $this->where = $this->parseWhere($sql);
     }
 
-    static public function query($sql) {
+    public function getResult() {
+        $results = resultsay();
+        foreach($this->from() as $url=>$resource) {
+            if(is_int($url)) {
+               $results[] = SimpleDigir::create($resource)->setResource("*")->getResult();
+            } else {
+                $q = SimpleDigir::create($url)->setResource($resource);
+                $filters = $this->where();
+                if(isset($filters['equals'])) {
+                    foreach($filters['equals'] as $field=>$value) {
+                        $q->addFilter($field,"equals",$value);
+                    }
+                }
+                if(isset($filters['like'])) {
+                    foreach($filters['like'] as $field=>$value) {
+                        $q->addFilter($field,"like",$value);
+                    }
+                }
+                $results[] = $q->getResult();
+            }
+        }
+        $records = array();
+        foreach($results as $result) {
+            foreach($result as $record) {
+                $records[] = $record;
+            }
+        }
+        $response = array() ;
+        foreach($records as $rec) {
+            $mappings = $this->fields();
+            $item = new StdClass ;
+            foreach($mappings as $field=>$map) {
+                $item->$field = $rec[$map];
+            }
+            $response[] = $item;
+        }
+        return $response;
+    }
+
+    public function parseFields($sql) {
+        $fields = array();
+        if(preg_match("@^SELECT (.*) FROM@i",$sql,$reg)) {
+            if($reg[1] != "*")  {
+                $parts = explode(",",$reg[1]);
+                foreach($parts as $fd) {
+                    $field = trim(str_replace("'","",$fd));
+                    if(preg_match("@([\w\d]+)( as ([\w\d]+))?@",$fd,$reg)){
+                        if(isset($reg[3])) {
+                            $fields[trim($reg[1])]=trim($reg[3]);
+                        } else {
+                            $fields[trim($reg[1])]=trim($reg[1]);
+                        }
+                    }
+                }
+
+            }
+        }
+        return $fields ;
+    }
+
+    public function parseFrom($sql) {
+        $from = array();
+        if(preg_match('@FROM ([^ ]+)( WHERE.*)?$@i',$sql,$reg)){
+            $parts = explode(",",$reg[1]);
+            foreach($parts as $part) {
+                if(preg_match("@^'([^']+)'.'([^']+)'$@",$part,$pair)){
+                    $url = trim($pair[1]);
+                    if(!isset($from[$url])) $from[$url] = array();
+                    $from[$url][] = trim($pair[2]);
+                } else if(preg_match("@^'([^']+)'$@",$part,$pair)){
+                    $url = trim($pair[1]);
+                    $from[] = $url;
+                }
+            }
+        }
+        return $from ;
+    }
+
+    public function parseWhere($sql) {
+        $where = array();
+        if(preg_match("@WHERE (.*)\$@i",$sql,$reg)) {
+            $combo = str_replace(",","AND",$reg[1]);
+            if(preg_match_all("@'?([^= ]+)'? *(=|like) *'([^']+)'( AND)?@i",$combo,$parts)) {
+                foreach($parts[0] as $k=>$v)  {
+                    if($parts[2][$k] == "=") {
+                        $op = "equals";
+                    } else {
+                        $op = "like";
+                    }
+                    $where[$op][trim($parts[1][$k])] = trim($parts[3][$k]);
+                }
+            }
+        }
+        return $where;
+    }
+
+    public function fields() {
+        return $this->fields ;
+    }
+
+    public function from() {
+        return $this->from;
+    }
+
+    public function where() {
+        return $this->where ;
+    }
+
+    static public function create($sql) {
+        return new DigirQuery($sql);
         $sql = trim($sql);
         $regex = "@^SELECT ([\w\d_\*-]+) FROM ([^ ]+)( WHERE .*)?$@i";
         if(preg_match($regex,$sql,$reg)) {
--- DigirQueryTest.php
+++ DigirQueryTest.php
@@ -5,30 +5,83 @@ include 'DigirQuery.php';
 class DigirQueryTest extends PHPUnit_Framework_TestCase {
 
     function testQuery() {
-        $sql =  "SELECT re-foo_bar123 FROM http://url.com";
-        $xml =  SimpleDigir::create("http://url.com")->setResource("re-foo_bar123")->makeRequest();
-        $this->assertEquals($xml,DigirQuery::query($sql)->makeRequest());
+        $sql =  "SELECT * FROM 'http://url.com'";
+        $query = DigirQuery::create($sql);
+        $from=array();
+        $from[] = "http://url.com";
+        $this->assertEquals($from,$query->from());
 
-        $sql =  "SELECT re-foo_bar123 FROM http://url.com WHERE foo like 'bar%'";
-        $xml =  SimpleDigir::create("http://url.com")->setResource("re-foo_bar123")->addFilter("foo","like","bar%")->makeRequest();
-        $this->assertEquals($xml,DigirQuery::query($sql)->makeRequest());
+        $sql =  "SELECT * FROM 'http://url.com'.'foo-bar'";
+        $query = DigirQuery::create($sql);
+        $fields = array();
+        $this->assertEquals($fields,$query->fields());
+        $from=array();
+        $from['http://url.com'][] = 'foo-bar';
+        $this->assertEquals($from,$query->from());
+        $where = array();
+        $this->assertEquals($where,$query->where());
 
-        $sql =  "SELECT re-foo_bar123 FROM http://url.com WHERE foo equals 'foo bar'";
-        $xml =  SimpleDigir::create("http://url.com")->setResource("re-foo_bar123")->addFilter("foo","equals","foo bar")->makeRequest();
-        $this->assertEquals($xml,DigirQuery::query($sql)->makeRequest());
+        $sql =  "SELECT field1 FROM 'http://url.com'.'foo-bar'";
+        $query = DigirQuery::create($sql);
+        $fields = array('field1'=>'field1');
+        $this->assertEquals($fields,$query->fields());
+        $from = array();
+        $from['http://url.com'][] = 'foo-bar';
+        $this->assertEquals($from,$query->from());
+        $where = array();
+        $this->assertEquals($where,$query->where());
 
-        $url = "http://dig.goog21.com.br:8081/hi-a/foo/bar.me/digir.php";
-        $sql =  "SELECT re-foo_bar123 FROM ".$url." WHERE foo equals 'bar'";
-        $xml =  SimpleDigir::create($url)->setResource("re-foo_bar123")->addFilter("foo","equals","bar")->makeRequest();
-        $this->assertEquals($xml,DigirQuery::query($sql)->makeRequest());
+        $sql =  "SELECT field1,Field2 ,field3 FROM 'http://url.com'.'foo-bar'";
+        $query = DigirQuery::create($sql);
+        $fields = array('field1'=>'field1','Field2'=>'Field2','field3'=>'field3');
+        $this->assertEquals($fields,$query->fields());
+        $from = array();
+        $from['http://url.com'][] = 'foo-bar';
+        $this->assertEquals($from,$query->from());
+        $where = array();
+        $this->assertEquals($where,$query->where());
 
-        $url = "http://dig.goog21.com.br:8081/hi-a/foo/bar.me/digir.php";
-        $sql =  "SELECT re-foo_bar123 FROM ".$url." WHERE foo equals 'bar', bar like 'foo%'";
-        $xml =  SimpleDigir::create($url)->setResource("re-foo_bar123")->addFilter("foo","equals","bar")->addFilter('bar','like','foo%')->makeRequest();
-        $this->assertEquals($xml,DigirQuery::query($sql)->makeRequest());
+        $sql =  "SELECT field1,Field2 as f2,field3 FROM 'http://url.com'.'foo-bar' WHERE field1='hi'";
+        $query = DigirQuery::create($sql);
+        $fields = array('field1'=>'field1','Field2'=>'f2','field3'=>'field3');
+        $this->assertEquals($fields,$query->fields());
+        $from = array();
+        $from['http://url.com'][] = 'foo-bar';
+        $this->assertEquals($from,$query->from());
+        $where = array('equals'=>array('field1'=>'hi'));
+        $this->assertEquals($where,$query->where());
 
-        $sql =  "SELECT * FROM http://url.com";
-        $this->assertEquals("*",DigirQuery::query($sql)->resource);
+        $sql =  "SELECT field1,Field2 ,field3 FROM 'http://url.com'.'foo-bar' WHERE field1='hi' AND field2='me'";
+        $query = DigirQuery::create($sql);
+        $from = array();
+        $from['http://url.com'][] = 'foo-bar';
+        $this->assertEquals($from,$query->from());
+        $where = array('field1'=>'hi','field2'=>'me');
+        $where = array('equals'=>array('field1'=>'hi','field2'=>'me'));
+        $this->assertEquals($where,$query->where());
+
+        $sql =  "SELECT field1,Field2 ,field3 FROM 'http://url.com'.'foo-bar' WHERE field1='hi' AND field2='me' AND field3='meme'";
+        $query = DigirQuery::create($sql);
+        $from = array();
+        $from['http://url.com'][] = 'foo-bar';
+        $this->assertEquals($from,$query->from());
+        $where = array('equals'=>array('field1'=>'hi','field2'=>'me','field3'=>'meme'));
+        $this->assertEquals($where,$query->where());
+
+        $sql =  "SELECT field1 FROM 'http://url.com'.'foo-bar','http://url2.com'.'rec2','http://url.com'.'rec2' WHERE field1='hi' AND field2='me'";
+        $query = DigirQuery::create($sql);
+        $from = array();
+        $from['http://url.com'][] = 'foo-bar';
+        $from['http://url2.com'][] = 'rec2';
+        $from['http://url.com'][] = 'rec2';
+        $this->assertEquals($from,$query->from());
+        $where = array('equals'=>array('field1'=>'hi','field2'=>'me'));
+        $this->assertEquals($where,$query->where());
+
+        $sql =  "SELECT * FROM 'http://url.com'.'foo-bar' WHERE field1='hi' AND field2 like 'me' AND field3='meme'";
+        $query = DigirQuery::create($sql);
+        $where = array('equals'=>array('field1'=>'hi','field3'=>'meme'),'like'=>array('field2'=>'me'));
+        $this->assertEquals($where,$query->where());
     }
 }
 ?>
